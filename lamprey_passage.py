4a24ed09466365c0fbc65176e2ab09c5888fb4ad
kemistree4 <46361264+kemistree4@users.noreply.github.com>
2019-01-05 08:58:05 -0800

    Add files via upload

--- lamprey_passage.py
+++ lamprey_passage.py
@@ -1,21 +1,20 @@
 # Imports
-# import picamera
-# import sys
-import time
+from picamera import PiCamera
+from time import sleep
 import RPi.GPIO as GPIO
-import os
-import json
 # import tinys3
 # import requests
 # from time import gmtime, strftime
 
 # Variables
+camera = PiCamera()
 PIN = 4                                # Pin we are using to read the door switch
 prev_input = 0             # Variable to track if trigger beam is broken
 
 # Setup the GPIO
 GPIO.setmode(GPIO.BCM)          # GPIO layout mode      
 GPIO.setup(PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP) #Setup the gpio pin we are reading from as an input
+
 # Loop checking the switch
 while True:
     # Read the switch value
@@ -24,11 +23,12 @@ while True:
     # If the last reading was low and this one high, record for 30 secs
     if not prev_input == input:
         print(time.time())
+        camera.start_recording ('/media/pi/Lexar/test_video/video.h264')
+        sleep(30)
+        camera.stop_recording
         if prev_input:
             print("recording video")
             
-            # raspivid -o video.h264 -t 30000
-
     # may also use?:
     #camera.start_recording('home/pi/video.h264)
     #sleep(30)
@@ -39,8 +39,8 @@ while True:
     # Wait slightly for debounce
     time.sleep(0.05)
 
-#code also needs"
+#code also needs to:
   #send an email to my work email with time of detection (beam is broken)
   #make log file of all detections
   #continue recording if beam is broken past initial 30 secs
-  #save videos to a USB drive#do nothing
+  #save videos to a USB drive
