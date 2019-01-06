3e9b0097f1885e33f8bcefb426029280dc71c03c
kemistree4 <46361264+kemistree4@users.noreply.github.com>
2019-01-06 08:21:47 -0800

    Add files via upload

--- lamprey_passage_gpio_trigger.py
+++ lamprey_passage_gpio_trigger.py
@@ -7,12 +7,11 @@ import datetime
 # Variables
 camera = PiCamera()
 PIN = 4                                # Pin we are using to read the IR break beam switch
-prev_input = 0             # Variable to track if trigger beam is broken
-now = time.localtime(time.time()) #Variable plugged into asci time to allow for readable date print out
-timestamp = datetime.datetime.now().strftime("%m%d%y_%H%M%S") #Variable to update name of video files with current date and time
+prev_input = 0                         # Variable to track if trigger beam is broken
+now = time.localtime(time.time())      #Variable plugged into asci time to allow for readable date print out
 
 # Setup the GPIO
-GPIO.setmode(GPIO.BCM)          # GPIO layout mode      
+GPIO.setmode(GPIO.BCM)                 # GPIO layout mode      
 GPIO.setup(PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP) #Setup the gpio pin we are reading from as a pullup input
 
 # Loop checking the switch
@@ -22,12 +21,14 @@ while True:
 
     # If the GPIO reading goes from high to low, record for 30 secs
     if input != 1:
+        timestamp = datetime.datetime.now().strftime("%m%d%y_%H%M%S") #Variable to update name of video files with current date and time
         print(time.asctime(now))
         camera.start_recording('/media/pi/Lexar/test_video/{}.h264'.format(timestamp)) #Recording video file to Lexar thumb drive
         camera.wait_recording(30)
         camera.stop_recording()
         
-    time.sleep(0.05)   # Wait slightly for debounce         
+    #Debounce wait
+    time.sleep(0.05)            
 
 #code also needs to:
   #send an email to my work email with time of detection (beam is broken)
