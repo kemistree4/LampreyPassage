acc8ee422f8faeafaa9d763ba80a604e7f4ecf89
kemistree4 <46361264+kemistree4@users.noreply.github.com>
2019-01-28 12:57:12 -0800

    Update lamprey_passage_code_with_logging.py

--- lamprey_passage_code_with_logging.py
+++ lamprey_passage_code_with_logging.py
@@ -36,8 +36,8 @@ while True:
         hdlr.setFormatter(formatter)
         #add the handler to the logger
         logger.addHandler(hdlr)
-        logger.error('Lamprey Detected!')
-        logger.info('Lamprey Detected!')
+        logger.error('Lamprey Detected1!')
+        logger.info('Lamprey Detected2!')
         GPIO.output(17,GPIO.HIGH)
         print(time.asctime(now))
         camera.start_recording('/media/pi/Lexar/test_video/{}.h264'.format(timestamp)) #Recording video file to Lexar thumb drive
