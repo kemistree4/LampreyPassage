38acde79d13afac30963a1bfea6c1efbae7386f0
kemistree4 <46361264+kemistree4@users.noreply.github.com>
2019-01-30 10:47:18 -0800

    Add files via upload

--- lamprey_passage_code_with_logging.py
+++ lamprey_passage_code_with_logging.py
@@ -13,13 +13,6 @@ log_time = datetime.datetime.now().strftime('%m%d%y')
 #create logger
 logger = logging.getLogger('myapp')
 logger.setLevel(logging.DEBUG) # log all escalated at and above DEBUG
-#add a file handler
-hdlr = logging.FileHandler('/media/pi/Lexar/log/{}.log'.format(log_time))
-# create a formatter and set the formatter for the handler
-formatter = logging.Formatter('%(asctime)s %(message)s')
-hdlr.setFormatter(formatter)
-#add the handler to the logger
-logger.addHandler(hdlr)
 
 # Setup the GPIO
 GPIO.setmode(GPIO.BCM)                 # GPIO layout mode      
@@ -36,10 +29,18 @@ while True:
         #define time format for filenames and log output
         now = time.localtime(time.time())      #Variable plugged into asci time to allow for readable date print out 
         timestamp = datetime.datetime.now().strftime("%m%d%y_%H%M%S") #Variable to update name of video files with current date and time
-        logger.info('Lamprey Detected!')
         GPIO.output(17,GPIO.HIGH)
         camera.start_recording('/media/pi/Lexar/test_video/{}.h264'.format(timestamp)) #Recording video file to Lexar thumb drive
         camera.wait_recording(30)
         camera.stop_recording()
+        #add a file handler
+        hdlr = logging.FileHandler('/media/pi/Lexar/log/{}.log'.format(log_time))
+        # create a formatter and set the formatter for the handler
+        formatter = logging.Formatter('%(asctime)s %(message)s')
+        hdlr.setFormatter(formatter)
+        #add the handler to the logger
+        logger.addHandler(hdlr)
+        logger.info('Lamprey Detected!')
+        logger.removeHandler(hdlr)
         GPIO.output(17,GPIO.LOW)
         time.sleep(0.05) #Debounce wait
