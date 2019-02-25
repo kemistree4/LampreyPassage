09d22c1dae2837296c8e7497da2fa11108fffb23
kemistree4 <46361264+kemistree4@users.noreply.github.com>
2019-02-25 10:21:37 -0800

    Update Lamprey_Passage_Post_Alt_Two.py

--- Lamprey_Passage_Post_Alt_Two.py
+++ Lamprey_Passage_Post_Alt_Two.py
@@ -1,24 +1,49 @@
+#Imports
 import io
 import time
 import picamera
 import RPi.GPIO as GPIO
+import logging
+import datetime
+import random
 
+#Variables
+BEAM_PIN = 4
+LED_PIN = 17
+
+#Setup the GPIO
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(17, GPIO.IN, GPIO.PUD_UP)
+GPIO.setup(BEAM_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(LED_PIN, GPIO.OUT)
+
+def write_video(stream):
+    print('Writing video!')
+    with stream.lock:
+        # Find the first header frame in the video
+        for frame in stream.frames:
+            if frame.frame_type == picamera.PiVideoFrameType.sps_header:
+                stream.seek(frame.position)
+                break
+        # Write the rest of the stream to disk
+        with io.open('/media/pi/Lexar/test_video/{}.h264'.format(timestamp), 'wb') as output:
+            output.write(stream.read())
 
 with picamera.PiCamera() as camera:
     stream = picamera.PiCameraCircularIO(camera, seconds=20)
     camera.start_recording(stream, format='h264')
-    GPIO.wait_for_edge(17, GPIO.FALLING)
-    time.sleep(30)
-    camera.stop_recording()
-    for frame in stream.frames:
-        if frame.header:
-            stream.seek(frame.position)
-            break
-    with io.open('/home/pi/Desktop/video.h264', 'wb') as output:
+    try:
         while True:
-            data = stream.read1()
-            if not data:
-                break
-            output.write(data)
+            camera.wait_recording(0.25)
+            input = GPIO.input(Beam_PIN)
+            if input != 1:
+                GPIO.output(17,GPIO.HIGH)
+                # Keep recording for 10 seconds and only then write the
+                # stream to disk
+                camera.wait_recording(10)
+                #define time format for filenames and log output
+                now = time.localtime(time.time())      #Variable plugged into asci time to allow for readable date print out 
+                timestamp = datetime.datetime.now().strftime("%m%d%y_%H%M%S") #Variable to update name of video files with current date and time
+                write_video(stream)
+    finally:
+        camera.stop_recording()
+        GPIO.output(17,GPIO.LOW)
