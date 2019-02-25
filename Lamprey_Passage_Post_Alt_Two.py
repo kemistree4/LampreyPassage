23f57a1e002c906a741d3ddea3e4c58d95e8d6ca
kemistree4 <46361264+kemistree4@users.noreply.github.com>
2019-02-25 09:47:42 -0800

    Create Lamprey_Passage_Post_Alt_Two.py

---
+++ Lamprey_Passage_Post_Alt_Two.py
@@ -0,0 +1,24 @@
+import io
+import time
+import picamera
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.IN, GPIO.PUD_UP)
+
+with picamera.PiCamera() as camera:
+    stream = picamera.PiCameraCircularIO(camera, seconds=20)
+    camera.start_recording(stream, format='h264')
+    GPIO.wait_for_edge(17, GPIO.FALLING)
+    time.sleep(30)
+    camera.stop_recording()
+    for frame in stream.frames:
+        if frame.header:
+            stream.seek(frame.position)
+            break
+    with io.open('/home/pi/Desktop/video.h264', 'wb') as output:
+        while True:
+            data = stream.read1()
+            if not data:
+                break
+            output.write(data)
