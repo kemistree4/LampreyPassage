e42a5da94433c2db4ffe312a133706d4fee954f6
kemistree4 <46361264+kemistree4@users.noreply.github.com>
2019-03-06 14:46:08 -0800

    Update Ians_timelapse.py

--- Ians_timelapse.py
+++ Ians_timelapse.py
@@ -1,9 +1,12 @@
 import time
 import picamera
+import datetime
 
 with picamera.PiCamera() as camera:
+    now = time.localtime(time.time())      #Variable plugged into asci time to allow for readable date print out 
+    timestamp = datetime.datetime.now().strftime("%m%d%y_%H%M%S") #Variable to update name of video files with current date and time
     camera.start_preview()
-    time.sleep(2)
-    for filename in camera.capture_continuous('img{counter:03d}.jpg'):
+    time.sleep(1)
+    for filename in camera.capture_continuous('/media/pi/C659-66D9/Lamprey_Images/{}.h264'.format(timestamp)):
         print('Captured %s' % filename)
-        time.sleep(300) # wait 5 minutes
+        time.sleep(2) # wait 2 secs
