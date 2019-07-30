875299a15fc1b35b61b109a0d6269754597b66f2
kemistree4 <46361264+kemistree4@users.noreply.github.com>
2019-07-30 15:03:28 -0700

    Add files via upload

--- lamprey_passage_code_with_logging.py
+++ lamprey_passage_code_with_logging.py
@@ -8,7 +8,6 @@ import logging
 # Variables
 camera = PiCamera()
 BEAM_PIN = 4                                # Pin we are using to read the IR break beam switch
-LED_PIN = 17                                # Pin we are using to activate the LED
 log_time = datetime.datetime.now().strftime('%m%d%y')
 #create logger
 logger = logging.getLogger('myapp')
@@ -17,7 +16,6 @@ logger.setLevel(logging.DEBUG) # log all escalated at and above DEBUG
 # Setup the GPIO
 GPIO.setmode(GPIO.BCM)                 # GPIO layout mode      
 GPIO.setup(BEAM_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP) #Setup the gpio pin we are reading from as a pullup input
-GPIO.setup(LED_PIN, GPIO.OUT)
 
 # Loop checking the switch
 while True:
@@ -29,9 +27,8 @@ while True:
         #define time format for filenames and log output
         now = time.localtime(time.time())      #Variable plugged into asci time to allow for readable date print out 
         timestamp = datetime.datetime.now().strftime("%m%d%y_%H%M%S") #Variable to update name of video files with current date and time
-        GPIO.output(17,GPIO.HIGH)
         #add a file handler
-        hdlr = logging.FileHandler('/media/pi/Lexar/log/{}.log'.format(log_time))
+        hdlr = logging.FileHandler('/media/pi/F8AF-129D/log_files/{}.log'.format(log_time))
         # create a formatter and set the formatter for the handler
         formatter = logging.Formatter('%(asctime)s %(message)s')
         hdlr.setFormatter(formatter)
@@ -39,8 +36,7 @@ while True:
         logger.addHandler(hdlr)
         logger.info('Lamprey Detected!')
         logger.removeHandler(hdlr)
-        camera.start_recording('/media/pi/Lexar/test_video/{}.h264'.format(timestamp)) #Recording video file to Lexar thumb drive
+        camera.start_recording('/media/pi/F8AF-129D/videos/{}.h264'.format(timestamp)) #Recording video file to Lexar thumb drive
         camera.wait_recording(30)
         camera.stop_recording()
-        GPIO.output(17,GPIO.LOW)
         time.sleep(0.05) #Debounce wait
