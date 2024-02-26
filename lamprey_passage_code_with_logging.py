2fd05ad02a643d86d6593c8ffcee4418fecfc7dd
Rikeem Sholes <46361264+kemistree4@users.noreply.github.com>
2024-02-26 07:39:51 -0800

    Update lamprey_passage_code_with_logging.py
    added in buffer to determine if conditions have cleared to reactivate sensors

--- lamprey_passage_code_with_logging.py
+++ lamprey_passage_code_with_logging.py
@@ -1,16 +1,20 @@
 # Imports
-from picamera import PiCamera
+from picamera2 import Picamera2
 import time
 import RPi.GPIO as GPIO
 import datetime 
 import schedule
+from picamera2.encoders import H264Encoder
+import statistics
 
 # Variables
-camera = PiCamera()
-BEAM_PIN = 4                                # Pin we are using to read the IR break beam switch
-log_time = datetime.datetime.now().strftime('%m%d%y')
+camera = Picamera2()
+BEAM_PIN = 17 # Pin we are using to read the IR break beam switch
+video_config = camera.create_video_configuration()
+camera.configure(video_config)
 videos_recorded = 0
 start_time = time.time()
+encoder = H264Encoder(10000000)
 
 # Setup the GPIO
 GPIO.setmode(GPIO.BCM)                 # GPIO layout mode      
@@ -19,42 +23,64 @@ GPIO.setup(BEAM_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP) #Setup the gpio pin we a
 def test_video():
     global videos_recorded
     now = time.localtime(time.time())      #Variable plugged into asci time to allow for readable date print out 
-    timestamp = datetime.datetime.now().strftime("%m%d%y_%H%M%S")
-    camera.start_recording('/media/pi/F8AF-129D/test_videos/{}.h264'.format(timestamp)) #Recording video file to Lexar thumb drive
-    camera.wait_recording(15)
+    timestamp = datetime.datetime.now().strftime("%m%d%y %H%M%S")
+    print("Recording test video")
+    camera.start_recording(encoder, '/media/counter/EEL CR VID/videos/test_video_{}.h264'.format(timestamp)) #Recording video file to Lexar thumb drive
+    time.sleep(15)
+    print("Test video complete")
     camera.stop_recording()
-    time.sleep(0.5)
+
+alarm_lst = [0 for _ in range(50)]
+
+def alarm_mod(new_value):
+    alarm_lst.insert(0, new_value)
+    alarm_lst.pop(-1)
+    return alarm_lst
 
 #Makes a test video everyday at 6 am and saves it to the test_video folder.
-schedule.every().day.at("06:00").do(test_video)
+schedule.every().day.at("16:48").do(test_video)
 
 # Loop checking the switch
 while True:
     # Read the switch value
     input = GPIO.input(BEAM_PIN)
+    alarm_lst = [0 for _ in range(50)]
     # If the GPIO reading goes from high to low, record for 30 secs
-    if input != 1:
-        time.sleep(2) # wait for 2 seconds
-        now = time.localtime(time.time())      #Variable plugged into asci time to allow for readable date print out 
-        timestamp = datetime.datetime.now().strftime("%m%d%y_%H%M%S") #Variable to update name of video files with current date and time
-        if input != 1:
-            # Check if 15 videos have been recorded in the last hour
-            if videos_recorded >= 15 and (time.time() - start_time) < 3600:  # 3600 seconds = 1 hour
-                # Wait for 30 cycles of input being equal to 1
-                count = 0
-                while count < 30:
-                    if GPIO.input(BEAM_PIN) == 1:
-                        count += 1
-                    else:
-                        count = 0
-                    time.sleep(1)  # Wait for 1 second before checking again
-                videos_recorded = 0  # Reset videos recorded count
-                start_time = time.time()  # Reset start time
-            else:
-                #define time format for filenames and log output
-                camera.start_recording('/media/pi/F8AF-129D/videos/{}.h264'.format(timestamp)) #Recording video file to Lexar thumb drive
-                camera.wait_recording(30)
-                camera.stop_recording()
-                videos_recorded += 1
-                time.sleep(0.05) #Debounce wait
-    schedule.run_pending()  # Run scheduled tasks
+    try:
+        if input != 0:
+            #print("Checkpoint 1")
+            time.sleep(2) # wait for 2 seconds
+            if input != 0:
+                #print("Checkpoint 2")
+                # Check if 15 videos have been recorded in the last hour
+                if videos_recorded >= 15 and (time.time() - start_time) < 3600:  # 3600 seconds = 1 hour
+                    # Wait for 30 cycles of input being equal to 1
+                    print("Too many files this hour. Deactivating until clear")
+                    alarm_mod(1)
+                    while statistics.mean(alarm_lst) > 0:
+                        if GPIO.input(BEAM_PIN) == 1 and statistics.mean(alarm_lst) > 0:
+                            #print("Checkpoint 3")
+                            alarm_mod(1)
+                            print(statistics.mean(alarm_lst))
+                        if GPIO.input(BEAM_PIN) == 0:
+                            alarm_mod(0)
+                            #print(statistics.mean(alarm_lst))
+                        if GPIO.input(BEAM_PIN) == 0 and statistics.mean(alarm_lst) == 0:
+                            print("Condition clear. Reactivating")
+                            break
+                        time.sleep(1)  # Wait for 1 second before checking again
+                    videos_recorded = 0  # Reset videos recorded count
+                    start_time = time.time()  # Reset start time
+                else:
+                    #define time format for filenames and log output
+                    now = time.localtime(time.time())      #Variable plugged into asci time to allow for readable date print out 
+                    timestamp = datetime.datetime.now().strftime("%m%d%y %H%M%S") #Variable to update name of video files with current date and time
+                    camera.start_recording(encoder, '/media/counter/EEL CR VID/videos/{}.h264'.format(timestamp)) #Recording video file to Lexar thumb drive
+                    time.sleep(60)
+                    camera.stop_recording()
+                    videos_recorded += 1
+                    time.sleep(0.05) #Debounce
+        schedule.run_pending()  # Run scheduled tasks
+    except:
+        continue
+        
